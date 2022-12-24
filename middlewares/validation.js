8dab860b40b9d781ab7d5e5c2bb974ef3b536591
Danya Kovan <mr.kovan_2002@mail.ru>
2022-12-24 14:39:19 +0100

    +middleware 'validation' + updateUser validator

---
+++ middlewares/validation.js
@@ -0,0 +1,12 @@
+const { Joi, celebrate } = require('celebrate');
+
+const updateUserValidation = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    email: Joi.string().required().email(),
+  }),
+});
+
+module.exports = {
+  updateUserValidation,
+};
--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,12 @@
 const userRouter = require('express').Router();
-const { celebrate, Joi } = require('celebrate');
+// const { celebrate, Joi } = require('celebrate');
 const {
   getUser,
   updateUser,
 } = require('../controllers/users');
+const {
+  updateUserValidation,
+} = require('../middlewares/validation');
 
 userRouter.get('/me', getUser);
-userRouter.patch(
-  '/me',
-  celebrate({
-    body: Joi.object().keys({
-      name: Joi.string().required().min(2).max(30),
-      email: Joi.string().required().email(),
-    }),
-  }),
-  updateUser,
-);
+userRouter.patch('/me', updateUserValidation, updateUser);
