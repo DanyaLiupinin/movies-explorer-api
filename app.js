ae343b279f20f9c9ff3c8f946cebb5109b1f88be
Danya Kovan <mr.kovan_2002@mail.ru>
2022-12-24 14:15:28 +0100

    +userSchema +movieSchema

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+// const mongoose = require('mongoose');
+
+const app = express();
+app.listen(3000);
+
+/*
+const bodyParser = require('body-parser');
+
+const { errors } = require('celebrate');
+
+mongoose.connect('mongodb://localhost:27017/mestodb');
+
+app.use(bodyParser.json());
+*/
---
+++ models/user.js
@@ -0,0 +1,30 @@
+// const bcrypt = require('bcryptjs');
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minLength: 2,
+    maxLength: 30,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator(email) {
+        return validator.isEmail(email);
+      },
+      message: '',
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+});
+
+module.exports = mongoose.model('user', userSchema);
