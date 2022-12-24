4f7fd66973acabb65e020e77add7a34cc65adb36
Danya Kovan <mr.kovan_2002@mail.ru>
2022-12-24 15:04:17 +0100

    +movies validation middlewares

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -1,5 +1,7 @@
 const { Joi, celebrate } = require('celebrate');
 
+const urlRegular = /https?:\/\/(www\.)?\d?\D{1,}#?/;
+
 const updateUserValidation = celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
@@ -13,7 +15,31 @@ const getUserValidation = celebrate({
   }),
 });
 
+const createMovieValidation = celebrate({
+  body: Joi.object().keys({
+    country: Joi.string().required(),
+    director: Joi.string().required(),
+    duration: Joi.number().required(),
+    year: Joi.number().required(),
+    description: Joi.string().required(),
+    image: Joi.string().required().pattern(urlRegular),
+    trailer: Joi.string().required().pattern(urlRegular),
+    nameRU: Joi.string().required(),
+    nameEN: Joi.string().required(),
+    thumbnail: Joi.string().required().pattern(urlRegular),
+    movieId: Joi.number().required(),
+  }),
+});
+
+const deleteMovieValidation = celebrate({
+  params: Joi.object().keys({
+    movieId: Joi.string().hex().length(24),
+  }),
+});
+
 module.exports = {
   updateUserValidation,
   getUserValidation,
+  createMovieValidation,
+  deleteMovieValidation,
 };
