14279a2cc59243b40923db0b03b856e5db10fa23
Danya Kovan <mr.kovan_2002@mail.ru>
2022-12-26 14:02:19 +0100

    +createUser controller

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,10 @@
+const bcrypt = require('bcryptjs');
+// const jwt = require('jsonwebtoken');
+
 const User = require('../models/user');
 const BadRequestError = require('../errors/BadRequestError');
 const NotFoundError = require('../errors/NotFoundError');
+const ConflictError = require('../errors/ConflictError');
 const status = require('../utils/status');
 
 const getUser = (req, res, next) => {
@@ -48,7 +52,42 @@ const updateUser = (req, res, next) => {
     });
 };
 
+const createUser = (req, res, next) => {
+  const {
+    name,
+    email,
+    password,
+  } = req.body;
+  bcrypt.hash(password, 10)
+    .then((hash) => {
+      User.create({
+        name,
+        email,
+        password: hash,
+      })
+        .then((user) => {
+          res.status(status.CREATED).send({
+            name: user.name,
+            email: user.email,
+            _id: user._id,
+          });
+        })
+        .catch((err) => {
+          if (err.name === 'ValidationError') {
+            next(new BadRequestError('Некорректные данные'));
+            return;
+          }
+          if (err.code === 11000) {
+            next(new ConflictError('Email уже используется'));
+            return;
+          }
+          next(err);
+        });
+    });
+};
+
 module.exports = {
   getUser,
   updateUser,
+  createUser,
 };
